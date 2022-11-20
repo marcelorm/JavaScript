cad9362ef1b1dda0c5ab5864cf004695ea4345bb
Marcelo Rodrigues Mendonça <marcelo.rodmendonca@gmail.com>
2022-11-20 11:48:44 -0300

    updates20112022

--- Node/index.js
+++ Node/index.js
@@ -1,3 +1,7 @@
+import chalk form 'chalk';
+
+console.log(chalk.blue('olá mundo'));
+
 console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/');
 console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/');
 console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/');
