1d6b38afc62033ae55bbad4d40dc99d9bda34925
Marcelo Rodrigues Mendonça <marcelo.rodmendonca@gmail.com>
2022-11-29 13:46:35 -0300

    alura-2

--- Node/aula-1/index.js
+++ Node/aula-1/index.js
@@ -1,6 +1,13 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+function extraiLinks(texto) {
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const capturas = [...texto.matchAll(regex)];
+  const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}));
+  return resultados;
+}
+
 function trataErro(erro) {
   console.log(erro);
   throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
@@ -12,22 +19,10 @@ async function pegaArquivo(caminhoDoArquivo) {
   try {
     const encoding = 'utf-8';
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-    console.log(chalk.green(texto));
+    console.log(extraiLinks(texto));
   } catch (erro) {
     trataErro(erro)
   }
 }
 
-
-// promises com then()
-
-// function pegaArquivo(caminhoDoArquivo) {
-//   const encoding = 'utf-8';
-//   fs.promises
-//     .readFile(caminhoDoArquivo, encoding)
-//     .then((texto) => console.log(chalk.green(texto)))
-//     .catch(trataErro)
-// }
-
 pegaArquivo('./arquivos/texto.md');
-pegaArquivo('./arquivos/');
